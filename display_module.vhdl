db277ef4917353e8bb52e1ea0b4a9d4f530ccfc5
gehrke0 <gehrke@udel.edu>
2018-05-01 16:29:55 -0400

    Should be good

---
+++ display_module.vhdl
@@ -0,0 +1,23 @@
+library IEEE;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity DISPLAY is
+	Port ( reg_data : in STD_LOGIC_VECTOR(7 downto 0);
+		   enable : in STD_LOGIC;
+		  
+end DISPLAY;
+
+architecture Behavioral of DISPLAY is 
+begin  
+	process(reg_data, enable) is
+	begin
+		if(enable = '0') then
+			report "0000";
+		else
+			report integer'image(to_integer(signed(reg_data)));
+		end if;
+	end process;
+
+
+end Behavioral;
